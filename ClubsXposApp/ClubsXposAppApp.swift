3941beba9dbc6475057689f59c1ff823d5f50467
Roman Volodin <rozarioagro@gmail.com>
2020-12-08 20:23:15 +0300

    Initial Commit

---
+++ ClubsXposApp/ClubsXposAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubsXposAppApp.swift
+//  ClubsXposApp
+//
+//  Created by roman on 08.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ClubsXposAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
