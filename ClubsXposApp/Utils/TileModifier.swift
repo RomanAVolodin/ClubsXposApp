3c922905f302422d4a631f0154631e9cda62d4e6
Roman Volodin <32717824+RomanAVolodin@users.noreply.github.com>
2020-12-09 19:50:53 +0300

    some stuff

---
+++ ClubsXposApp/Utils/TileModifier.swift
@@ -0,0 +1,23 @@
+//
+//  TileModifier.swift
+//  ClubsXposApp
+//
+//  Created by Roman Vol on 09.12.2020.
+//
+
+import SwiftUI
+
+extension View {
+    func asTile() -> some View {
+        modifier(TileModifier())
+    }
+}
+
+struct TileModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .background(Color.white)
+            .cornerRadius(5.0)
+            .shadow(color: .init(.sRGB, white: 0.8, opacity: 1), radius: 2, x: 0, y: 2)
+    }
+}
